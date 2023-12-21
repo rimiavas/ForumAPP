03b042737c05fd1f6bd5fe318fab79e4a77b4268
rimiavas <139592563+rimiavas@users.noreply.github.com>
2023-12-21 19:26:43 +0000

    Update create_db.sql

--- create_db.sql
+++ create_db.sql
@@ -25,6 +25,7 @@ CREATE TABLE user_topics (
   id INT PRIMARY KEY AUTO_INCREMENT,
   user_id INT,
   topic_id INT,
+  created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
   FOREIGN KEY (user_id) REFERENCES users(id),
   FOREIGN KEY (topic_id) REFERENCES topics(id)
 );
